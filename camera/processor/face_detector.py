8debb000ec52d72bb3e97d6157a2a325f5e9257f
Goji <goji2100@gmail.com>
2018-07-23 20:41:13 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -41,8 +41,9 @@ class FaceDetector(object):
         # 顔の位置を描画する
         n = 0
         for (x,y,w,h) in faces:
+            n++
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.putText(frame,++n,(x,y), font, 4,(255,255,255),2,cv2.LINE_AA)
+            cv2.putText(frame, n, (x,y), font, 4,(255,255,255),2,cv2.LINE_AA)
 
         # frameを戻り値として返す
         return frame
